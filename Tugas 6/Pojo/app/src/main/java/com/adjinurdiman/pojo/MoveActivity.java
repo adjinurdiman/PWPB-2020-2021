ccea1231ca366e3e104c7033a8f9bde9d5321de3
adjinurdiman <adjinurdiman@gmail.com>
2020-08-23 23:21:39 +0700

    Tugas 6

---
+++ Tugas 6/Pojo/app/src/main/java/com/adjinurdiman/pojo/MoveActivity.java
@@ -0,0 +1,14 @@
+package com.adjinurdiman.pojo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+}
